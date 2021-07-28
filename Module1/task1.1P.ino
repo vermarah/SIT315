8f9241b7ed4fe29cf468cd87a22c302537fd9e63
vermarah <63293838+vermarah@users.noreply.github.com>
2021-07-28 18:58:53 +1000

    1.1P

--- Module1/task1.1P.ino
+++ Module1/task1.1P.ino
@@ -2,69 +2,43 @@
 //
 
 
-const int pingPin = 2;                //setting up the pins for the sensors
-const int led_pin = 13;
+const int motion_pin = 2;                //setting up the pins for the sensors
+const int led_pin = 12;
 const int buzz_pin = 7;
 
 void setup()
 {
-  
+   
   pinMode(led_pin, OUTPUT);            //setting up the output/input pins for the sensors
   pinMode(buzz_pin, OUTPUT);
-  Serial.begin(9600);                  
+  //pinMode(motion_pin, INPUT_PULLUP); 
+   
+  attachInterrupt(digitalPinToInterrupt(motion_pin), isr, CHANGE);  //change used for intrupting whenever the pin changes value
+  Serial.begin(9600);
 }
 
 void loop()
 {
-  										 //in inches and centimeters
-  long duration, inches, cm;             //variables for distance and duration for conversion to microseconds
-  
-  
-  pinMode(pingPin, OUTPUT);               // for getting a clean high pulse, gave a short LOW pulse beforehand
-  digitalWrite(pingPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(pingPin, HIGH);            //similarly repeating the process again
-  delayMicroseconds(5);
-  digitalWrite(pingPin, LOW);
-
-  pinMode(pingPin, INPUT);
-  duration = pulseIn(pingPin, HIGH);
+  //nothing here
+}
 
   
-  inches = microsecondsToInches(duration);              //converting the time into distance
-  cm = microsecondsToCentimeters(duration);  
-  
-  if (cm <= 80)                      //conditional statement as if the cm distance is less than 80, the buzzer should sound
-  {
-    digitalWrite(led_pin, HIGH);     //and the led glows
-    delay(500);
-    digitalWrite(led_pin, LOW);
-    delay(500);
-    
-    tone(buzz_pin, 1000);            //buzzer sounds
+
+void isr()
+{
+  	if (digitalRead(motion_pin) == HIGH) //if motion is sensed, we see the led light and hear the buzzer 
+    {
+    									//turning the LED on and buzzer sounds
+    digitalWrite(led_pin, HIGH);
+    tone(buzz_pin, 100);
+    Serial.println("Sensed the motion");
   }
-  else                               //else condition
+  else 
   {
-    digitalWrite(led_pin, LOW);
+    digitalWrite(led_pin, LOW);     //if motion is not sensed, we turn off the led light and the buzzer 
     noTone(buzz_pin);
+    Serial.println("Nothing there");   //output on the serial monitor
   }
 
-  Serial.print(inches);             //printing the values on the serial monitor
-  Serial.print("in, ");
-  Serial.print(cm);
-  Serial.print("cm");
-  Serial.println();
-
-  delay(100);
 }
 
-long microsecondsToInches(long microseconds) {
-  // According to Parallax's datasheet, there are 73.746 microseconds per inch (i.e. sound travels at 1130 feet per second).
-  // This gives the distance travelled
-  return microseconds / 74 / 2;
-}
-
-long microsecondsToCentimeters(long microseconds) {
-  // The speed of sound is 29 microseconds per centimeter divide by 2 as back and forth
-  return microseconds / 29 / 2;
-}
